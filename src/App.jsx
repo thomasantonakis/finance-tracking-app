dd0656c8e0c3bfcde17f49376caa3f4173a67d4d
Thomas Antonakis <thomas.antonakis@gmail.com>
2026-01-31 00:02:48 +0200

    scroll to top when navigating

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import Layout from "./Layout.jsx";
 import Home from "./Pages/Home.jsx";
@@ -9,6 +9,12 @@ import Charts from "./Pages/Charts.jsx";
 import Settings from "./Pages/Settings.jsx";
 
 export default function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [pathname]);
+
   return (
     <Layout>
       <Routes>
